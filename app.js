55b2f1a8f5c9fc896dff047f498dd42db862a178
Aritra1704 <aritra1704@gmail.com>
2019-04-11 17:04:06 +0530

    Error handling

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const request = require('request')
  
-const url = 'https://api.darksky.net/forecast/62a0bdddff6d2306187480b1af18b9c7/37.8267,-122.4233'
+const url = 'https://api.darksky.net/forecast/62a0bdddff6d2306187480b1af18b9c7/17.461795, 78.353516'
 
-request({url: url}, (error, response) => {
-    const data = JSON.parse(response.body)
-    console.log(data.currently)
+request({url: url, json: true}, (error, response) => {
+    // const data = JSON.parse(response.body)
+    // console.log(data.currently)
+
+    // console.log(response.body.currently)
+
+    if(error) {
+        console.log('Unable to connect to weather service');
+    } else if(response.body.error) {
+        console.log('Unable to find location')
+    } else {
+        console.log(response.body.daily.data[0].summary + ' It\'s currently ' + response.body.currently.temperature + ' degrees out. With ' + response.body.currently.precipProbability + ' percent chance of rain.')
+    }
+    
 })
